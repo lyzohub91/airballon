41bfe26a923260022d56a8e8e3c61b729e44975a
Andrii Lyzogub <andrii.lyzogub@eleks.com>
2019-09-04 11:47:33 +0300

    Year changed and name added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log("\nNice work! Your first application have been launched.");
 console.log("Now, I need to know your name, type it below: ");
+console.log("Andrii Lyzogub");
 
 let stdin = process.openStdin();
 
@@ -8,7 +9,7 @@ stdin.addListener("data", (txt) => {
         console.log("\nHave a nice JS trip! Bye-bye");
         stdin.end();
     } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
+        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2019. I am sure you'll become great in your job.`);
         console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
     }   
   });
