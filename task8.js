21edceecfaf9c5fc05527abe781b69b6382d1920
Andrii Lyzogub <andrii.lyzogub@eleks.com>
2019-10-15 11:49:08 +0300

    task8

---
+++ task8.js
@@ -0,0 +1,11 @@
+4 XPath locators;
+//*[@id="p-logo"]/a - Wiki logo
+//div[@id="articlecount"] - Div element for Article count
+//*[@id="mp-dyk"]/ul/li[4]  -- 4-th item from "Did you know…" list
+// count(//*[@id="p-interaction"]/div)  -- Count of ‘Interaction’ elements
+
+4 CSS selectors;
+#p-logo a -- Wiki logo
+div#articlecount --Div element for Article count
+#mw-head #left-navigation [href="/wiki/Main_Page"] -- Main page link
+#mp-dyk > ul li:nth-child(2) -- 2-nd item from "Did you know…" list
